1b3ee6d69f889df60feeda51bd647f9afc5719a8
prodevmod <rosandrox12@gmail.com>
2026-08-11 12:41:06 +0300

    stemnet seo final

--- app.py
+++ app.py
@@ -304,7 +304,8 @@ def register():
 limiter = Limiter(
     app=app,
     key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"]
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://"
 )
 
 @app.route("/login", methods=["GET", "POST"])
