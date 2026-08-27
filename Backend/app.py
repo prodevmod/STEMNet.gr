ec711fea4be967d8c96e53b1fb8bc76f0a4095ee
prodevmod <rosandrox12@gmail.com>
2026-08-27 18:42:41 +0300

    enabled rate limit

--- Backend/app.py
+++ Backend/app.py
@@ -91,10 +91,9 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 limiter = Limiter(
     app=app,
     key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"],
+    default_limits=["50 per hour"],
     storage_uri="memory://"
 )
-app.config["RATELIMIT_ENABLED"] = False
 
 # Utility Functions for Email Verification
 def get_serializer():
