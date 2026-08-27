a6bd8fbeb4caa1877de05709c6d35ac6c86730d2
prodevmod <rosandrox12@gmail.com>
2026-08-28 02:07:23 +0300

    made more deploying friendly vite

--- Frontend/client/vite.config.js
+++ Frontend/client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000', // Change to :80 if Flask runs on port 80
+        target: 'http://0.0.0.0:80',
         changeOrigin: true,
       },
     },
